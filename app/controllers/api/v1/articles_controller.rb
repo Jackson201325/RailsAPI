19632a38e47a5c904433851168bf9cca3f5506e2
Jackson Huang <jacksonh201325@gmail.com>
2019-09-21 00:43:23 -0400

    Created Articles API

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+        @articles = Article.order('created_at DESC')
+        render json: { status: 'SUCCESS',
+                       message: 'Loaded Articles',
+                       data: @articles },
+               status: :ok
+      end
+    end
+  end
+end
